64211d99a90c15dfb2258b25a7ae443820b6f20c
Clare Lindley <clare.lindley@gmail.com>
2020-02-28 15:07:02 +0000

    rename server variable; emit chat msg back to client

--- src/index.js
+++ src/index.js
@@ -5,18 +5,20 @@ import {createServer} from 'http';
 import io from 'socket.io';
 const app = express();
 const http = createServer(app);
-const socket = io(http);
+const io_server = io(http);
 
 // This is a route handler for the root URL '/'
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
 
-socket.on('connection', (socket) => {
+io_server.on('connection', (socket) => {
     console.log('a user connected');
 
+    // when we get the message from the client, we send it back to display on screen
     socket.on('chat message', (msg) => {
         console.log(`message: ${msg}`);
+        io_server.emit('chat message', msg);
     });
 
     socket.on('disconnect', () => console.log('user disconnected'));
