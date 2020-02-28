d1d7c42803bb22ac6e5b0fd6756c66e6a34c4b57
Clare Lindley <clare.lindley@gmail.com>
2020-02-28 15:27:44 +0000

    Add npm watch to run babel build automatically

--- src/index.js
+++ src/index.js
@@ -2,23 +2,24 @@
 
 import express from 'express';
 import {createServer} from 'http';
-import io from 'socket.io';
+import SocketIO from 'socket.io';
 const app = express();
 const http = createServer(app);
-const io_server = io(http);
+const io = SocketIO(http);
 
 // This is a route handler for the root URL '/'
+// this is a comment
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
 
-io_server.on('connection', (socket) => {
+io.on('connection', (socket) => {
     console.log('a user connected');
 
     // when we get the message from the client, we send it back to display on screen
     socket.on('chat message', (msg) => {
         console.log(`message: ${msg}`);
-        io_server.emit('chat message', msg);
+        io.emit('chat message', msg);
     });
 
     socket.on('disconnect', () => console.log('user disconnected'));
