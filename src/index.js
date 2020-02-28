f5c6a9e2db229ab4c257c69d283c465557e64cf8
Clare Lindley <clare.lindley@gmail.com>
2020-02-28 14:06:15 +0000

    emit first chat message back to server W00t! I AM L33T HAXORRRRR :D

--- src/index.js
+++ src/index.js
@@ -14,6 +14,11 @@ app.get('/', (req, res) => {
 
 socket.on('connection', (socket) => {
     console.log('a user connected');
+
+    socket.on('chat message', (msg) => {
+        console.log(`message: ${msg}`);
+    });
+
     socket.on('disconnect', () => console.log('user disconnected'));
 });
 
